abd1923cd00acc81312166718fe5bdc14392eae7
KC Thomas <thisiskcthomas@gmail.com>
2020-09-22 17:00:28 +0100

    Removed students variable and created method to use user input to get student names.

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex Delarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "to finish, just hit return twice"
+  students = []
+  name = get.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    nem = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The Students of Villains Academy"
@@ -27,6 +27,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
